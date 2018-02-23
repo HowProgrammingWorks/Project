50eccb8761448ccf363fe6c037eb92d1dadf4727
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-23 23:55:35 +0200

    Submodules and remove from cache

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 'use strict';
 
-const metasync = require('metasync');
+const lib = {};
+['submodule1', 'submodule2', 'submodule3'].forEach(name => {
+  const sub = require('./lib/' + name + '.js');
+  Object.assign(lib, sub);
+});
+
+console.log('We combined submodules:');
+console.log(Object.keys(lib).join(', '));
+
+const metasync1 = require('metasync');
+const metasyncPath = require.resolve('metasync');
+delete require.cache[metasyncPath];
+const metasync2 = require('metasync');
+if (metasync1 === metasync2) {
+  console.log('Cache not reloaded');
+} else {
+  console.log('Cache reloaded');
+}
 
 console.log('We use metasync:');
-console.log(Object.keys(metasync));
+console.log(Object.keys(metasync1).join(', '));
